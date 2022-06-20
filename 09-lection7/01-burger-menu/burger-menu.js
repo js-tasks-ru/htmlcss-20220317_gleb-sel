b1db49b75847dcf21ae015fe868dc3d00ab59b87
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2022-06-20 20:02:56 +0300

    solve burger menu

--- 09-lection7/01-burger-menu/burger-menu.js
+++ 09-lection7/01-burger-menu/burger-menu.js
@@ -2,10 +2,16 @@ const menu = document.getElementById('burger-menu');
 const menuClose = document.getElementById('burger-menu-close');
 const menuOpen = document.getElementById('burger-menu-open');
 
+window.onload = () => {
+    document.body.classList.remove("preload");
+  };
+
 if (menuOpen) {
     menuOpen.addEventListener('click', function() {
         if (menu) {
             menu.setAttribute('data-open', '');
+            menuOpen.classList.add('burger-menu__button_hidden')
+            menuClose.classList.remove('burger-menu__button_hidden');
         }
     });
 }
@@ -14,6 +20,8 @@ if (menuClose) {
     menuClose.addEventListener('click', function() {
         if (menu) {
             menu.removeAttribute('data-open');
+            menuOpen.classList.remove('burger-menu__button_hidden');
+            menuClose.classList.add('burger-menu__button_hidden');
         }
     });
 }
